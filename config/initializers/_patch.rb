2232d148fe3ab5e7e3a4a56c8dd8b1cbe91bc837
serge <net57@free.fr>
2017-04-25 23:29:46 +0200

    patch ProxyPacRb Encoding

---
+++ config/initializers/_patch.rb
@@ -0,0 +1,20 @@
+# patch to correct utf8 string wrongly encoded as  ASCII-8BIT
+# tested on linyx with ruby 2.2.7-p470
+module ProxyPacRb
+  # Encodes strings as UTF-8
+  module Encoding
+    def encode(string)
+      if string.encoding.name == 'ASCII-8BIT'
+        data = string.dup
+        data.force_encoding('UTF-8')
+
+        unless data.valid_encoding?
+          fail ::Encoding::UndefinedConversionError, "Could not encode ASCII-8BIT data #{string.dump} as UTF-8"
+        end
+      else
+        data = string.encode('UTF-8')
+      end
+      data
+    end
+  end
+end
