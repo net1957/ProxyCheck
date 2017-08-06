5105e788980bff52cbce65c387a1c94bc18a419a
serge <net57@free.fr>
2017-08-06 11:26:46 +0200

    follow rubocop advices

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resource :scripts, only: [:new, :create]
+  resource :scripts, only: %i[new create]
 
   root 'scripts#new'
 end
