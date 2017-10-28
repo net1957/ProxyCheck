0a0e64f7d72cc7418b185603397cd2bc0b09d156
serge <net57@free.fr>
2017-10-28 22:10:38 +0200

    Patch ProxyPacRb to disallow use of proxypac on file system (for security)

--- config/initializers/_patch.rb
+++ config/initializers/_patch.rb
@@ -1,4 +1,4 @@
-# patch to correct utf8 string wrongly encoded as ASCII-8BIT
+# patch to correct utf8 string wrongly encoded as ASCII-8BIT and suppress file system proxy load
 # tested on linux with ruby 2.2.7-p470
 module ProxyPacRb
   # Encodes strings as UTF-8
@@ -15,6 +15,31 @@ module ProxyPacRb
         data = string.encode('UTF-8')
       end
       data
+    end
+  end
+
+  # Dump Proxy pac to file system
+  class ProxyPacLoader
+    def initialize
+      @loaders = []
+      @loaders << ProxyPacStringLoader.new
+      @loaders << ProxyPacUriLoader.new
+      # @loaders << ProxyPacFileLoader.new
+      @loaders << ProxyPacNullLoader.new
+
+      @default_loader = -> { ProxyPacNullLoader.new }
+    end
+  end
+  # Handle proxy pac == nil
+  class ProxyPacNullLoader
+    def load(proxy_pac)
+      proxy_pac.type = :null
+
+      raise StandardError, 'URL or script is not valid'
+    end
+
+    def suitable_for?(proxy_pac)
+      proxy_pac.nil?
     end
   end
 end
