47aa6b58e325c80b9bae97dd8e94e43f05f7acae
net57 <net57@free.fr>
2022-01-11 20:01:02 +0100

    follow rubocop advices

--- config/initializers/_patch.rb
+++ config/initializers/_patch.rb
@@ -10,7 +10,9 @@ module ProxyPacRb
         data = string.dup
         data.force_encoding('UTF-8')
 
-        raise(::Encoding::UndefinedConversionError, "Could not encode ASCII-8BIT data #{string.dump} as UTF-8") unless data.valid_encoding?
+        unless data.valid_encoding?
+          raise(::Encoding::UndefinedConversionError, "Could not encode ASCII-8BIT data #{string.dump} as UTF-8")
+        end
       else
         data = string.encode('UTF-8')
       end
