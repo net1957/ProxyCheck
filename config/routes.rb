fbf880e0b46b18be1de570a141ee30d3baebac43
net57 <net57@free.fr>
2022-01-11 18:56:00 +0100

    refactor toutes to allow refresh on view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :scripts, only: %i[new create]
+  get '/scripts', to: 'scripts#new'
+
   resource :optimize, only: %i[new create]
+  get '/optimize', to: 'optimize#new'
 
   # to respond to health checks.
   get '/health', to: 'health#index'
