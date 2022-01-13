ecacd06b004af144b0c08a3ad35bec7e4ef2f0b5
net57 <net57@free.fr>
2022-01-13 17:49:40 +0100

    correct file name of InputAddons module
    adjust Todo

--- lib/components/simple_form/input_addons.rb
+++ lib/components/simple_form/input_addons.rb
@@ -4,7 +4,7 @@
 # permit to pass a html string wrapped bay a  div .control
 module InputAddons
   def addons(_wrapper_options = nil)
-    # INFO: security: we assume that addons is not a user input
+    # INFO: security: we assume that options[:addons] is not a user input
     template.content_tag(:div, options[:addons]&.to_s&.html_safe, class: 'control') # rubocop:disable Rails/OutputSafety
   end
 end
